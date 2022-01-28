b78623426cc9f6ff334e8d05a3a553559a3c9568
jakobdubeau1 <94392098+jakobdubeau@users.noreply.github.com>
2022-01-28 01:54:41 +0000

    setups (images not final)

---
+++ js/titleScene.js
@@ -0,0 +1,44 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This is the Title Scene
+
+class TitleScene extends Phaser.Scene {
+  constructor () {
+    super({ key: 'titleScene' })
+    
+    this.titleSceneBackgroundImage = null
+    this.titleSceneText = null
+    this.titleSceneTextStyle = { font: '200px Times', fill: '#fde4b9', align: 'center' }
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Title Scene')
+      this.load.image('titleSceneBackground', './title.png')
+      //this.load.audio('music', './music.mp3')
+  }
+
+  create (data) {
+
+    this.titleSceneBackgroundImage = this.add.sprite(0, 0, 'titleSceneBackground').setScale(1)
+    this.titleSceneBackgroundImage.x = 1920 / 4
+    this.titleSceneBackgroundImage.y = 1080 / 4
+
+    //this.titleSceneText = this.add.text(1920 / 2, (1080 / 2) + 350, 'Space Aliens', //this.titleSceneTextStyle).setOrigin(0.5)
+  }
+
+  update (time, delta) {
+    if (time > 6000) {
+      this.scene.switch('menuScene')
+    }
+  }
+}
+
+export default TitleScene

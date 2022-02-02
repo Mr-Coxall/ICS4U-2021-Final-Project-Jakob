729734f3a34241fa8bd4a95c8cf154da65e70132
jakobdubeau1 <94392098+jakobdubeau@users.noreply.github.com>
2022-02-02 04:35:52 +0000

    test

--- js/game.js
+++ js/game.js
@@ -44,24 +44,3 @@ game.scene.add('menuScene', menuScene)
 game.scene.add('gameScene', gameScene)
 
 game.scene.start('startScreen')
-
-    var Food = new Phaser.Class({
-
-        Extends: Phaser.GameObjects.Image,
-
-        initialize:
-
-        function Food (scene, x, y)
-        {
-            Phaser.GameObjects.Image.call(this, scene)
-
-            this.setTexture('food');
-            this.setPosition(x * 16, y * 16);
-            this.setOrigin(0);
-
-            this.total = 0;
-
-            scene.children.add(this);
-        }
-
-    })
--- js/gameScene.js
+++ js/gameScene.js
@@ -32,187 +32,49 @@ class GameScene extends Phaser.Scene {
   }
 
 create (data) {
-  //this.snake = this.physics.add.sprite(1920 / 2, 1080 - 100, 'snake')
-    var Food = new Phaser.Class({
+  this.snake = this.physics.add.sprite(800 / 2, 600 - 100, 'snake')
+  this.food = this.physics.add.sprite(200, 100, 'food')
 
-        Extends: Phaser.GameObjects.Image,
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        initialize:
-
-        function Food (scene, x, y)
-        {
-            Phaser.GameObjects.Image.call(this, scene)
-
-            this.setTexture('food');
-            this.setPosition(x * 16, y * 16);
-            this.setOrigin(0);
-
-            this.total = 0;
-
-            scene.children.add(this);
-        }
-
-    })
-
-      var Snake = new Phaser.Class({
-
-        initialize:
-
-        function Snake (scene, x, y)
-        {
-            this.headPosition = new Phaser.Geom.Point(x, y)
-
-            this.body = scene.add.group()
-
-            this.head = this.body.create(x * 32, y * 32, 'body')
-            this.head.setOrigin(0)
-
-            this.alive = true
-
-            this.speed = 100
-
-            this.moveTime = 0
-
-            this.heading = RIGHT
-            this.direction = RIGHT
-        },
-
-        update: function (time)
-        {
-            if (time >= this.moveTime)
-            {
-                return this.move(time)
-            }
-        },
-
-        faceLeft: function ()
-        {
-            if (this.direction === UP || this.direction === DOWN)
-            {
-                this.heading = LEFT
-            }
-        },
-
-        faceRight: function ()
-        {
-            if (this.direction === UP || this.direction === DOWN)
-            {
-                this.heading = RIGHT
-            }
-        },
-
-        faceUp: function ()
-        {
-            if (this.direction === LEFT || this.direction === RIGHT)
-            {
-                this.heading = UP
-            }
-        },
-
-        faceDown: function ()
-        {
-            if (this.direction === LEFT || this.direction === RIGHT)
-            {
-                this.heading = DOWN
-            }
-        },
-
-        move: function (time)
-        {
-            switch (this.heading)
-            {
-                case LEFT:
-                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x - 1, 0, 40)
-                    break
-
-                case RIGHT:
-                    this.headPosition.x = Phaser.Math.Wrap(this.headPosition.x + 1, 0, 40)
-                    break
-
-                case UP:
-                    this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y - 1, 0, 30)
-                    break
-
-                case DOWN:
-                    this.headPosition.y = Phaser.Math.Wrap(this.headPosition.y + 1, 0, 30)
-                    break
-            }
-
-            this.direction = this.heading
-
-            //  Update the body segments
-            Phaser.Actions.ShiftPosition(this.body.getChildren(), this.headPosition.x * 16, this.headPosition.y * 16, 1)
-
-            //  Update the timer ready for the next movement
-            this.moveTime = time + this.speed
-
-            return true
-        }
-
-    });
-
-    snake = new Snake(this, 8, 8)
-    food = new Food(this, 3, 4);
-    //  Create our keyboard controls
-    cursors = this.input.keyboard.createCursorKeys()
-  
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
 update (time, delta) {
 
-      if (!snake.alive)
-    {
-        return;
-    }
 
-    /**
-    * Check which key is pressed, and then change the direction the snake
-    * is heading based on that. The checks ensure you don't double-back
-    * on yourself, for example if you're moving to the right and you press
-    * the LEFT cursor, it ignores it, because the only valid directions you
-    * can move in at that time is up and down.
-    */
-    if (cursors.left.isDown)
-    {
-        snake.faceLeft();
-    }
-    else if (cursors.right.isDown)
-    {
-        snake.faceRight();
-    }
-    else if (cursors.up.isDown)
-    {
-        snake.faceUp();
-    }
-    else if (cursors.down.isDown)
-    {
-        snake.faceDown();
-    }
+    if (cursors.left.isDown) {
 
-    snake.update(time);
-    //const keyLeftObj = this.input.keyboard.addKey('LEFT')
-    //const keyRightObj = this.input.keyboard.addKey('RIGHT')
+        
 
-    //if (keyLeftObj.isDown === true) {
-     // this.snake.body.velocity.x = -200
-     // this.snake.body.velocity.y = 0
+      this.snake.body.velocity.x = -200
+      this.snake.body.velocity.y = 0
       
       
       //this.snake.x -= 15
       //if (this.snake.x < 0) {
         //this.snake.x = 0
-     // }
+      }
   
 
-  //  if (keyRightObj.isDown === true) {
-    //  this.snake.body.velocity.x = 200
-      //this.snake.body.velocity.y = 0
+    else if (cursors.right.isDown) {
+      this.snake.body.velocity.x = 200
+      this.snake.body.velocity.y = 0
 
       //this.snake.x += 15
       //if (this.snake.x > 1920) {
         //this.snake.x = 1920
-    //}
-  }
-}
+      }
+      if (cursors.up.isDown) {
+        this.snake.body.velocity.x = 0
+        this.snake.body.velocity.y = -200
+      }
 
+      else if (cursors.down.isDown) {
+        this.snake.body.velocity.x = 0
+        this.snake.body.velocity.y = 200
+      }
+  }
+
+}
 export default GameScene
